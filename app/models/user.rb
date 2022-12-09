3b82a1c932b11063aec5e75f8e0786c5524d26ed
rodgersochieng <sirrodgers21@gmail.com>
2022-12-09 23:24:00 +0300

    updated validations in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
     # belongs_to :trainer
     # has_many :tutorials ,through: :trainer
-
-    # validates :email, presence: true, uniqueness: true
-    # validates :username, presence: true, uniqueness: true
+    
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true
 end
