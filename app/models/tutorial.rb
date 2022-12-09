791db10ad7ffb56888ce77bbd143e1db2bfb6990
rodgersochieng <sirrodgers21@gmail.com>
2022-12-09 23:27:56 +0300

    updated tutorial validations in models

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,5 +1,5 @@
 class Tutorial < ApplicationRecord
     belongs_to :trainer
     
-    # validates :description, length: {minimum:20, maximum:1000}
+    validates :description, length: {minimum:20, maximum:1000}
 end
