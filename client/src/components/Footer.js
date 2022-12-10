130f2a4a86b22e9628a32e0351c2c96e9c748a2a
rodgersochieng <sirrodgers21@gmail.com>
2022-12-10 13:14:47 +0300

    removed postgres changed to sqlite3

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 
-function Footer (){
-return(
-    <div style={{marginTop:"40px",borderTop:"2px solid #000",textAlign:"center",padding:"15px"}}>Moringa School - Phase-4 - Group A Project  </div>
-);
- }
- export default Footer;
-    
+function Footer() {
+  return (
+    <div
+      style={{
+        marginTop: "40px",
+        borderTop: "2px solid #000",
+        textAlign: "center",
+        padding: "15px",
+      }}
+    >
+      {" "}
+    </div>
+  );
+}
+export default Footer;
